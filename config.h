f8d62cf4c20af74d7eec9baa37679e9ec597c767
Mark Fickett <mark.fickett@gmail.com>
2018-02-10 17:22:37 -0500

    Add skeleton of using 7 segment LED and LowPower, initializing new pins.

--- config.h
+++ config.h
@@ -1,33 +1,21 @@
 // Pin-number and behavior configuration.
 
-// Not the onboard status LED, which is used by the micro-SD card and obscured
-// in this application.
-#define PIN_STATUS_LED 6
-
-// DHT-22 Temperature/humidity sensor: https://learn.adafruit.com/dht
-// Draws .05mA between reads, 1.66mA while reading.
-#define PIN_DHT22  8
-
-// Increase this interval if DHT22 says DHT_ERROR_TOOQUICK (7). 5s is OK.
 // This value is passed to Narcoleptic as an int, so must be < INT_MAX (32767).
-#define SLEEP_INTERVAL_MILLIS (30 * 1000)
-
-// Whether to read voltage of the solar panel. Turn this off if the charge
-// controller does not tie the panel's ground to battery/load ground.
-//#define READ_PV_VOLTAGE
+#define SLEEP_INTERVAL_MILLIS (10 * 1000)
 
 // 12v (battery) power supply for the 9V regulator powering the camera.
-#define PIN_CAMERA_POWER_SUPPLY 9
-// Red wire to camera via opto-isolator.
-#define PIN_CAMERA_POWER_ON 3
-// Yellow wire to camera via opto-isolator.
-#define PIN_CAMERA_SHUTTER 2
-// A normally-open switch. When closed momentarily, the camera stays on until
-// it is closed momentarily again.
-#define PIN_CAMERA_STAY_ON_SWITCH 4
+#define PIN_CAMERA_POWER_SUPPLY 5
+
+// To camera via opto-isolator.
+#define PIN_CAMERA_FOCUS 6
+#define PIN_CAMERA_EXPOSE 7
 // Sense the memory-access LED on the camera to detect when an exposure is done
 // and written to storage.
-#define PIN_CAMERA_MEMORY_ACCESS A2
+#define PIN_CAMERA_ACTIVITY_SENSE A1
+
+#define PIN_AMBIENT_LIGHT_SENSE A2
+
+#define PIN_BATTERY_SENSE A3
 
 // Threshold for the analog reading for the camera memory-access LED. LED on
 // (writing) is typically 0, and off (done) is typically 300.
@@ -39,52 +27,17 @@
 // exposure (1 minute).
 #define CAMERA_FAILSAFE_WAIT_COUNT_LIMIT 1200
 
-// Whether to use fast mode (frequent photos whenever it's light), or schedule
-// mode (photos at specific times).
-#define FAST_MODE
-#define PHOTO_INTERVAL_FAST_SECONDS (10 * 60)
-// Only take frequent photos when the solar module is providing some voltage
-// (as a proxy for only taking daytime photos) and when the battery has enough
-// charge.
-#ifdef READ_PV_VOLTAGE
-#define FAST_MODE_PV_THRESHOLD_V 10.0
-#else
-#define FAST_MODE_PV_THRESHOLD_V -1
-#endif
 // 11.8v is generally considered a lower limit for safe SLA discharge. Allow a
 // little buffer, since we want to keep logging even if battery voltage is too
 // low to take photos. solarnavigator.net/battery_charging.htm
-#define FAST_MODE_VCC_THRESHOLD_V 12.0
-// Do not take frequent pictures at night. Night is any hour (UTC) between the
-// two specified, excluding both. So with 0 and 8 (8PM and 4AM EDT),
-//   0:44 is day
-//   1:00 is night
-//   7:58 is night
-//   8:00 is day
-// Values based on summer solstice for the lat/lng of interest.
-// http://aa.usno.navy.mil/data/docs/RS_OneDay.php
-#define FAST_MODE_NIGHT_AFTER_HOUR 0
-#define FAST_MODE_NIGHT_BEFORE_HOUR 8
 
-#define PIN_DIVIDED_VCC A0
-#define PIN_DIVIDED_PV  A1
 // Resistor values for battery and photovoltaics. Each pair may be scaled
 // together arbitrarily (ex: 4.7 and 1.2 or 47 and 12).
-// The voltage dividers draw about 0.02mA each.
-#define VCC_DIVIDER_SRC (1.000 + 0.992)
-#define VCC_DIVIDER_GND 0.272
-#define PV_DIVIDER_SRC  (0.997 + 0.996)
-#define PV_DIVIDER_GND  0.272
-#define AREF 2.048
+#define AREF 5.0
 
-#define PIN_SPI_CHIP_SELECT_REQUIRED 10
-// The SD library requires, for SPI communication:
-// 11 MOSI (master/Arduino output, slave/SD input)
-// 12 MISO (master input, slave output)
-// 13 CLK (clock)
-// Current draw is card dependant. forum.arduino.cc/index.php?topic=149504.15
-// Crucial 4GB class 2: 0.06mA sleep between writes, 7.5mA writes.
-// SanDisk 4GB class 4: 0.10mA sleep, 4.8mA writes.
-// SanDisk ExtremePlus 32GB: 4.32mA sleep, 8mA writes.
-// Kinsington 16GB: 16mA sleep, 22mA writes.
+#define PIN_WAKE_BUTTON 2
+#define PIN_CHANGE_BUTTON 3
 
+#define PIN_7SEG_POWER 4
+
+#define BUTTON_DEBOUNCE_MILLIS 100
--- timelapse.ino
+++ timelapse.ino
@@ -1,383 +1,180 @@
 /**
- * Run time lapse camera, monitoring environment and battery stats.
- *
- * Overall, the system draws (@12v) 0.7mA while idle, 14mA during data
- * sampling/writes, and 200mA-2A when triggering the camera.
+ * Run time lapse camera.
  */
 
 #include "Wire.h"
-// Using https://github.com/ringerc/Arduino-DHT22 for Pro Micro 3.3v .
-// Turning off timers for sleep requires replacing millis() in DHT22.cpp with
-// Narcoleptic.millis().
-#include "DHT22.h"
 // Using https://github.com/adafruit/RTClib
 #include "RTClib.h"
-// Using https://github.com/greiman/SdFat
-#include "SdFat.h"
-// Using https://code.google.com/p/narcoleptic/source/browse/
-#include "Narcoleptic.h"
+// Adafruit 7-segment LED
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+// https://github.com/rocketscream/Low-Power
+#include "LowPower.h"
 
+// pin numbers and timings
 #include "config.h"
-
-struct SensorData {
-  DateTime now;
-  float humidityPercent;
-  float temperatureCelsius;
-  int dividedVcc;
-  int dividedPhotoVoltaic;
-};
-
-struct SensorData sensorData;
-DHT22 dht(PIN_DHT22);
-DHT22_ERROR_t dhtError;
-
-uint32_t nextPhotoSeconds;
-// In schedule mode, take photos at three predefined times during the day.
-// Repeat the first hour +24 as the last value so there's always an hour in this
-// list greater than the current hour, for ease of calculating a time in the
-// future. These must have intervals of >1h.
-uint8_t scheduleModeHours[] = {
-    4,   // midnight
-    13,  // mid-morning: 8AM (winter) or 9AM (summer)
-    21,  // late afternoon but always before sunset: 4 (winter) or 5PM (summer)
-    28
-};
 
 // Chronodot https://www.adafruit.com/products/255 and guide
 // learn.adafruit.com/ds1307-real-time-clock-breakout-board-kit/wiring-it-up
 // It draws 0.1mA consistently while 3.3 vcc is supplied.
 RTC_DS1307 rtc;
 
-char buf[128]; // filenames and data log lines
-SdFat sd;
-boolean sdError;
-SdFile logFile;
+// learn.adafruit.com/adafruit-led-backpack/0-dot-56-seven-segment-backpack
+Adafruit_7segment display = Adafruit_7segment();
+
+volatile unsigned long buttonPressTimesMillis[2] = {0L, 0L};
+volatile boolean buttonPressed[2] = {false, false};
+
+void handleWakePressed() { handleButtonPress(0); }
+bool consumeWakePressed() { return consumeButtonPress(0); }
+void handleChangePressed() { handleButtonPress(1); }
+bool consumeChangePressed() { return consumeButtonPress(1); }
+void handleButtonPress(int i) {
+  unsigned long t = millis();
+  if (!buttonPressed[i] &&
+      t - buttonPressTimesMillis[i] > BUTTON_DEBOUNCE_MILLIS) {
+    buttonPressed[i] = true;
+    buttonPressTimesMillis[i] = t;
+  }
+}
+bool consumeButtonPress(int i) {
+  if (buttonPressed[i]) {
+    buttonPressed[i] = false;
+    return true;
+  }
+  return false;
+}
 
 void setup() {
   Serial.begin(57600);
   // This must be called before analogRead if voltage is applied to the AREF
   // pin, or else the board may be damaged.
   analogReference(EXTERNAL);
-  pinMode(PIN_STATUS_LED, OUTPUT);
-  digitalWrite(PIN_STATUS_LED, HIGH);
 
-  Wire.begin();
   rtc.begin();
   //setTime();
-  setUpCameraPins();
 
-  readTime();
+  pinMode(PIN_CAMERA_ACTIVITY_SENSE, INPUT);
+  pinMode(PIN_AMBIENT_LIGHT_SENSE, INPUT);
+  pinMode(PIN_BATTERY_SENSE, INPUT);
 
-  // Schedule the next photo in the past, for the most recent 10-minute
-  // increment. This way a photo will be taken immediately, and subsequent
-  // photos will be taken at :00, :10, :20, etc.
-  nextPhotoSeconds = sensorData.now.unixtime();
-  nextPhotoSeconds -= nextPhotoSeconds % PHOTO_INTERVAL_FAST_SECONDS;
+  pinMode(PIN_CAMERA_POWER_SUPPLY, OUTPUT);
+  digitalWrite(PIN_CAMERA_POWER_SUPPLY, LOW);
 
-  sdError = false;
-  if (!sd.begin(PIN_SPI_CHIP_SELECT_REQUIRED, SPI_QUARTER_SPEED)) {
-    sdError = true;
-    sd.initErrorPrint();
-    for (int i = 0; i < 10; i++) {
-      digitalWrite(PIN_STATUS_LED, HIGH);
-      delay(50);
-      digitalWrite(PIN_STATUS_LED, LOW);
-      delay(100);
-    }
-  }
-  digitalWrite(PIN_STATUS_LED, LOW);
+  pinMode(PIN_WAKE_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_CHANGE_BUTTON, INPUT_PULLUP);
+
+  pinMode(PIN_CAMERA_FOCUS, OUTPUT);
+  digitalWrite(PIN_CAMERA_FOCUS, LOW);
+  pinMode(PIN_CAMERA_EXPOSE, OUTPUT);
+  digitalWrite(PIN_CAMERA_EXPOSE, LOW);
+
+  pinMode(PIN_7SEG_POWER, OUTPUT);
+  digitalWrite(PIN_7SEG_POWER, HIGH);
+  display.begin(0x70 /* default I2C address */); // also sets max brightness
+  display.clear();
+  display.writeDigitRaw(0, 0b01110100); // h
+  display.writeDigitRaw(1, 0b01111001); // E
+  display.writeDigitRaw(3, 0b00111000); // L
+  display.writeDigitRaw(4, 0b10111111); // O.
+  display.writeDisplay();
+  delay(1000);
+
+  attachInterrupt(
+      digitalPinToInterrupt(PIN_WAKE_BUTTON), handleWakePressed, FALLING);
+  attachInterrupt(
+      digitalPinToInterrupt(PIN_CHANGE_BUTTON), handleChangePressed, FALLING);
 }
 
 void setTime() {
   DateTime now = DateTime(__DATE__, __TIME__); // time at compilation
-  now = now + TimeSpan(0, 4, 0, 0); // EST => UTC
+  now = now + TimeSpan(0, 5, 0, 0); // EDT => UTC
   rtc.adjust(now);
 }
 
-void setUpCameraPins() {
-  pinMode(PIN_CAMERA_POWER_SUPPLY, OUTPUT);
-  pinMode(PIN_CAMERA_POWER_ON, OUTPUT);
-  pinMode(PIN_CAMERA_SHUTTER, OUTPUT);
-  pinMode(PIN_CAMERA_STAY_ON_SWITCH, INPUT_PULLUP);
-}
-
+bool colonBlink = false;
 void loop() {
-  readTime();
-  readTemperatureAndHumidity();
-  readVoltages();
-  printData();
-  writeData();
-  if (scheduleNextPhotoGetIsTimeForPhoto()) {
-    triggerCamera();
-  }
-  if (digitalRead(PIN_CAMERA_STAY_ON_SWITCH) == LOW) {
-    digitalWrite(PIN_STATUS_LED, HIGH);
-    digitalWrite(PIN_CAMERA_POWER_SUPPLY, HIGH);
-    delay(100);
-    digitalWrite(PIN_CAMERA_POWER_ON, HIGH);
-    delay(100);
-    while (true) {
-      Serial.println(F("Waiting on."));
-      delay(1000);
-      if (digitalRead(PIN_CAMERA_STAY_ON_SWITCH) == LOW) {
-        Serial.println(F(" done"));
-        break;
-      }
-    }
-    delay(100);
-    digitalWrite(PIN_CAMERA_POWER_ON, LOW);
-    delay(1000);
-    digitalWrite(PIN_CAMERA_POWER_SUPPLY, LOW);
-    digitalWrite(PIN_STATUS_LED, LOW);
-  }
+  DateTime now = rtc.now();  // now.year(), .hour(), etc
+  display.clear();
+  display.println(now.hour() * 100 + now.minute());
+  display.drawColon(colonBlink = !colonBlink);
+  display.writeDisplay();
+  delay(1000);
 
-  lowPowerSleepMillis(SLEEP_INTERVAL_MILLIS);
-}
-
-bool scheduleNextPhotoGetIsTimeForPhoto() {
-  uint32_t t = sensorData.now.unixtime();
-  bool isTimeForPhoto;
-
-#ifdef FAST_MODE
-  if (t < nextPhotoSeconds) {
-    isTimeForPhoto = false;
-  } else if (sensorData.now.hour() > FAST_MODE_NIGHT_AFTER_HOUR &&
-      sensorData.now.hour() < FAST_MODE_NIGHT_BEFORE_HOUR) {
-    Serial.print("Skipping night photo at UTC hour ");
-    Serial.println(sensorData.now.hour());
-    isTimeForPhoto = false;
-  } else {
-    Serial.println("Time for photo.");
-    int pvReading = sensorData.dividedPhotoVoltaic;
-    float pvVoltage =
-        (pvReading * AREF * (PV_DIVIDER_SRC + PV_DIVIDER_GND))
-        / (1023 * PV_DIVIDER_GND);
-    int vccReading = sensorData.dividedVcc;
-    float vccVoltage =
-        (vccReading * AREF * (VCC_DIVIDER_SRC + VCC_DIVIDER_GND))
-        / (1023 * VCC_DIVIDER_GND);
-    if (pvVoltage < FAST_MODE_PV_THRESHOLD_V) {
-      Serial.print("PV voltage ");
-      Serial.print(pvVoltage);
-      Serial.println("v too low, skipping photo.");
-      isTimeForPhoto = false;
-    } else if (vccVoltage < FAST_MODE_VCC_THRESHOLD_V) {
-      Serial.print("VCC voltage ");
-      Serial.print(vccVoltage);
-      Serial.println("v too low, skipping photo.");
-      isTimeForPhoto = false;
-    } else {
-      isTimeForPhoto = true;
-    }
-  }
-  while (nextPhotoSeconds <= t) {
-    nextPhotoSeconds += PHOTO_INTERVAL_FAST_SECONDS;
-  }
-#else
-  isTimeForPhoto = t >= nextPhotoSeconds;
-  // Find the next scheduled photo time.
-  if (isTimeForPhoto) {
-    int i;
-    uint8_t currentHour = sensorData.now.hour();
-    while (scheduleModeHours[i] <= currentHour) {
-      i++;
-    }
-    // DateTime::unixtime implicitly allows hours > 24 (no bounds checking).
-    DateTime nextPhotoDateTime(
-        sensorData.now.year(),
-        sensorData.now.month(),
-        sensorData.now.day(),
-        scheduleModeHours[i]);
-    nextPhotoSeconds = nextPhotoDateTime.unixtime();
-    Serial.print(F("Next: "));
-    printDateTime(nextPhotoDateTime);
-    Serial.println();
-  }
-#endif
-
-  return isTimeForPhoto;
-}
-
-void readTime() {
-  sensorData.now = rtc.now();
-}
-
-void readTemperatureAndHumidity() {
-  if ((dhtError = dht.readData()) == DHT_ERROR_NONE) {
-    sensorData.humidityPercent = dht.getHumidity();
-    sensorData.temperatureCelsius = dht.getTemperatureC();
-  } else {
-    Serial.print(F("DHT22 Error: "));
-    Serial.println(dhtError);
-    sensorData.humidityPercent = NAN;
-    sensorData.temperatureCelsius = NAN;
-  }
-}
-
-void readVoltages() {
-  sensorData.dividedVcc = analogRead(PIN_DIVIDED_VCC);
-#ifdef READ_PV_VOLTAGE
-  sensorData.dividedPhotoVoltaic = analogRead(PIN_DIVIDED_PV);
-#else
-  sensorData.dividedPhotoVoltaic = 0;
-#endif
-}
-
-void printDateTime(const DateTime& t) {
-  Serial.print(t.unixtime());
-  Serial.print(F(" "));
-  Serial.print(t.year(), DEC);
-  Serial.print('/');
-  Serial.print(t.month(), DEC);
-  Serial.print('/');
-  Serial.print(t.day(), DEC);
-  Serial.print(' ');
-  Serial.print(t.hour(), DEC);
-  Serial.print(':');
-  Serial.print(t.minute(), DEC);
-  Serial.print(':');
-  Serial.print(t.second(), DEC);
-}
-
-void printData() {
-  Serial.print(F("t: "));
-  printDateTime(sensorData.now);
-
-  Serial.print(F("\tTemperature: "));
-  if (isnan(sensorData.temperatureCelsius)) {
-    Serial.print(F("NaN"));
-  } else {
-    Serial.print(sensorData.temperatureCelsius);
-    Serial.print(F(" C"));
-  }
-  Serial.print(F("\tHumidity: "));
-  if (isnan(sensorData.humidityPercent)) {
-    Serial.print(F("NaN"));
-  } else {
-    Serial.print(sensorData.humidityPercent);
-    Serial.print(F(" %"));
-  }
-
-  printVoltage(
-      F("\tVcc "), sensorData.dividedVcc, VCC_DIVIDER_SRC, VCC_DIVIDER_GND);
-  printVoltage(
-      F("\tPV "), sensorData.dividedPhotoVoltaic, PV_DIVIDER_SRC,
-      PV_DIVIDER_GND);
-
-  Serial.println();
-}
-
-void printVoltage(
-    const __FlashStringHelper* prefix,
-    int analogReading,
-    float rToSource,
-    float rToGround) {
-  Serial.print(prefix);
-  Serial.print(analogReading);
-  Serial.print(F("/1023 "));
-  // 3.3 * A0 / 1023 = Vmeasured
-  Serial.print(analogReading * AREF / 1024);
-  Serial.print(F("v measured => "));
-  // Vsrc / (R1 + R2) = Vmeasured / R2
-  // Vsrc = (3.3 * A0 * (R1 + R2)) / (1023 * R2)
-  Serial.print(
-      (analogReading * AREF * (rToSource + rToGround))
-      / (1023 * rToGround));
-  Serial.print(F("v"));
-}
-
-void writeData() {
-  sprintf(
-      buf,
-      "%02d%02d%02d.csv",
-      sensorData.now.year(),
-      sensorData.now.month(),
-      sensorData.now.day());
-  if (!sdError && !logFile.open(buf, O_CREAT | O_WRITE | O_AT_END)) {
-    sdError = true;
-    sd.errorPrint("Opening logFile failed. ");
-  }
-
-  obufstream ob(buf, sizeof(buf));
-  ob << setprecision(2)
-      << sensorData.now.unixtime()
-      << ","
-      << sensorData.temperatureCelsius
-      << ","
-      << sensorData.humidityPercent
-      << ","
-      << sensorData.dividedVcc
-      << ","
-      << sensorData.dividedPhotoVoltaic
-      << "\n";
-  logFile.print(buf);
-
-  if (!sdError && !logFile.sync() || logFile.getWriteError()) {
-    sdError = true;
-    sd.errorPrint("Writing logFile failed. ");
-  }
-  logFile.close();
-}
-
-void triggerCamera() {
-  Serial.println(F("Triggering camera."));
+  /*
   digitalWrite(PIN_CAMERA_POWER_SUPPLY, HIGH);
-  delay(100);
-  digitalWrite(PIN_CAMERA_POWER_ON, HIGH);
-  // Wait for camera initialization.
-  waitForCameraMemoryAccess();
-
-  Serial.println(F(" shutter"));
-  digitalWrite(PIN_CAMERA_SHUTTER, HIGH);
-  delay(50);
-  digitalWrite(PIN_CAMERA_SHUTTER, LOW);
-  waitForCameraMemoryAccess();
-
-  Serial.println(F(" power off"));
-  digitalWrite(PIN_CAMERA_POWER_ON, LOW);
-  delay(100);
+  delay(1000);
   digitalWrite(PIN_CAMERA_POWER_SUPPLY, LOW);
+  delay(2000);
+  */
+
+  if (consumeWakePressed()) {
+    display.clear();
+    display.print(1337);
+    display.writeDisplay();
+    delay(1000);
+  }
+  if (consumeChangePressed()) {
+    display.clear();
+    display.writeDigitRaw(0, 0b00111001); // C
+    display.writeDigitRaw(1, 0b01110100); // h
+    display.writeDigitRaw(3, 0b11011100); // a
+    display.writeDigitRaw(4, 0b01101111); // g
+    display.writeDisplay();
+    delay(1000);
+  }
+
+  /*
+  digitalWrite(PIN_CAMERA_FOCUS, HIGH);
+  delay(1000);
+  digitalWrite(PIN_CAMERA_FOCUS, LOW);
+  */
+
+  /*
+  display.clear();
+  display.print(analogRead(PIN_CAMERA_ACTIVITY_SENSE));
+  display.writeDisplay();
+  delay(1000);
+  */
+
+  display.clear();
+  // 500 = normal room, 900 = dim, 950 = dark
+  display.print(analogRead(PIN_AMBIENT_LIGHT_SENSE));
+  display.writeDisplay();
+  delay(1000);
+
+  /*
+  digitalWrite(PIN_CAMERA_EXPOSE, HIGH);
+  digitalWrite(PIN_CAMERA_EXPOSE, LOW);
+  */
+
+  /*
+  display.clear();
+  display.print(analogRead(PIN_BATTERY_SENSE));
+  display.writeDisplay();
+  delay(1000);
+  */
+
+  lowPowerSleep();
 }
 
-void waitForCameraMemoryAccess() {
-  int a;
-  int failsafeCount = 0;
-  // Wait for the exposure to finish and the camera to start writing.
-  Serial.println(F(" access start wait"));
-  while (((a = analogRead(PIN_CAMERA_MEMORY_ACCESS)) < CAMERA_ACCESS_THRESHOLD)
-      && failsafeCount < CAMERA_FAILSAFE_WAIT_COUNT_LIMIT) {
-    delay(CAMERA_ACCESS_WAIT_INTERVAL_MS);
-    failsafeCount++;
-  }
-  // Wait for for the camera to finish writing the image.
-  Serial.println(F(" access end wait"));
-  while (((a = analogRead(PIN_CAMERA_MEMORY_ACCESS)) > CAMERA_ACCESS_THRESHOLD)
-      && failsafeCount < CAMERA_FAILSAFE_WAIT_COUNT_LIMIT) {
-    delay(CAMERA_ACCESS_WAIT_INTERVAL_MS);
-    failsafeCount++;
-  }
-  if (failsafeCount >= CAMERA_FAILSAFE_WAIT_COUNT_LIMIT) {
-    Serial.println(F("memory access wait timeout"));
-  }
-}
+void lowPowerSleep() {
+  display.clear();
+  display.writeDisplay();
+  display.setBrightness(0);
+  digitalWrite(PIN_7SEG_POWER, LOW);
 
-void lowPowerSleepMillis(int millis) {
-  // Finish sending any debug messages before disabling timers.
-  Serial.flush();
+  // During power-down, the "wake" button interrupt will be registered, and will
+  // immediately cancel power-down mode.
+  // With adafru.it/1065 step-down transformer (6.5-32v in, 5v out), the board
+  // draws 1.3mA asleep. (The power regulator draws 1-2mA, so accounts for most
+  // if not all of this.)
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_ON);
 
-  Narcoleptic.disableTimer1();
-  Narcoleptic.disableTimer2();
-  Narcoleptic.disableSerial();
-  Narcoleptic.disableADC();
-  Narcoleptic.disableWire();
-  Narcoleptic.disableSPI();
-
-  Narcoleptic.delay(millis);
-
-  Narcoleptic.enableTimer1();
-  Narcoleptic.enableTimer2();
-  Narcoleptic.enableSerial();
-  Narcoleptic.enableADC();
-  Narcoleptic.enableWire();
-  Narcoleptic.enableSPI();
+  digitalWrite(PIN_7SEG_POWER, HIGH);
+  // Give the display time to wake up. Without an explicit delay, it takes
+  // 2-4s to reconnect [retries/backoff?].
+  delay(50);
+  // Must call begin() again after power cycling the display.
+  display.begin(0x70 /* default I2C address */); // also sets max brightness
 }
